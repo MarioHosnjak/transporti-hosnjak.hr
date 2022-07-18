20b275b16bd4e438ce4803af2eb3fbb2f1cd422d
Mario Hosnjak <mario.hosnjak009@gmail.com>
2022-07-18 19:04:13 +0200

    commit 18.07

--- js/projectCounter.js
+++ js/projectCounter.js
@@ -1,3 +1,16 @@
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
+
+
+
 $.fn.jQuerySimpleCounter = function( options ) {
     var settings = $.extend({
         start:  0,
@@ -21,7 +34,24 @@ $.fn.jQuerySimpleCounter = function( options ) {
 };
 
 
-$('#number1').jQuerySimpleCounter({end: 13000,duration: 3000});
+var counterDiv = document.querySelector('.item');
+
+document.addEventListener('scroll', function () {
+    
+    if(counterDiv && isInViewport(counterDiv)){
+        console.log("DIV JE U VIEWPORTU!")
+        $('#number1').jQuerySimpleCounter({end: 13000,duration: 3000});
+        $('#number2').jQuerySimpleCounter({end: 40,duration: 3500});
+        $('#number3').jQuerySimpleCounter({end: 17,duration: 3500});
+        $('#number4').jQuerySimpleCounter({end: 50,duration: 3200});
+        counterDiv = undefined;
+    }
+}, {
+    passive: true
+});
+
+
+/* $('#number1').jQuerySimpleCounter({end: 13000,duration: 3000});
 $('#number2').jQuerySimpleCounter({end: 40,duration: 3500});
 $('#number3').jQuerySimpleCounter({end: 17,duration: 3500});
-$('#number4').jQuerySimpleCounter({end: 50,duration: 3200});
+$('#number4').jQuerySimpleCounter({end: 50,duration: 3200}); */
