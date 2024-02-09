582b621dfd5da00fc23deda733a1aa8e2295cbb7
Mario Hosnjak <mario.hosnjak009@gmail.com>
2024-02-09 11:53:49 +0100

    Changed 2023 to 24, changed phone number to international format

--- js/projectCounter.js
+++ js/projectCounter.js
@@ -1,54 +1,59 @@
 function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
-    );
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
+$.fn.jQuerySimpleCounter = function (options) {
+  var settings = $.extend(
+    {
+      start: 0,
+      end: 100,
+      easing: "swing",
+      duration: 400,
+      complete: "",
+    },
+    options
+  );
 
+  var thisElement = $(this);
 
-$.fn.jQuerySimpleCounter = function( options ) {
-    var settings = $.extend({
-        start:  0,
-        end:    100,
-        easing: 'swing',
-        duration: 400,
-        complete: ''
-    }, options );
-
-    var thisElement = $(this);
-
-    $({count: settings.start}).animate({count: settings.end}, {
-        duration: settings.duration,
-        easing: settings.easing,
-        step: function() {
-            var mathCount = Math.ceil(this.count);
-            thisElement.text(`+${mathCount.toLocaleString('hi-IN')}`);
-        },
-        complete: settings.complete
-    });
+  $({ count: settings.start }).animate(
+    { count: settings.end },
+    {
+      duration: settings.duration,
+      easing: settings.easing,
+      step: function () {
+        var mathCount = Math.ceil(this.count);
+        thisElement.text(`+${mathCount.toLocaleString("hi-IN")}`);
+      },
+      complete: settings.complete,
+    }
+  );
 };
 
+var counterDiv = document.querySelector(".item");
 
-var counterDiv = document.querySelector('.item');
-
-document.addEventListener('scroll', function () {
-    
-    if(counterDiv && isInViewport(counterDiv)){
-        $('#number1').jQuerySimpleCounter({end: 16000,duration: 3000});
-        $('#number2').jQuerySimpleCounter({end: 40,duration: 3500});
-        $('#number3').jQuerySimpleCounter({end: 19,duration: 3500});
-        $('#number4').jQuerySimpleCounter({end: 50,duration: 3200});
-        counterDiv = undefined;
+document.addEventListener(
+  "scroll",
+  function () {
+    if (counterDiv && isInViewport(counterDiv)) {
+      $("#number1").jQuerySimpleCounter({ end: 16000, duration: 3000 });
+      $("#number2").jQuerySimpleCounter({ end: 40, duration: 3500 });
+      $("#number3").jQuerySimpleCounter({ end: 20, duration: 3500 });
+      $("#number4").jQuerySimpleCounter({ end: 50, duration: 3200 });
+      counterDiv = undefined;
     }
-}, {
-    passive: true
-});
-
+  },
+  {
+    passive: true,
+  }
+);
 
 /* $('#number1').jQuerySimpleCounter({end: 13000,duration: 3000});
 $('#number2').jQuerySimpleCounter({end: 40,duration: 3500});
